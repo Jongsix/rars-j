09a6fff67f68a4b6c19a6cea4b14c8c448c7be34
Pedro Sanders <sanderspedro@gmail.com>
2020-09-20 21:55:57 -0400

    Removed run.sh file

--- run.sh
+++
@@ -1,4 +0,0 @@
-docker-compose down --remove-orphans
-rm -rf ~/.jitsi-meet-cfg
-mkdir -p ~/.jitsi-meet-cfg/{web/letsencrypt,transcripts,prosody/config,prosody/prosody-plugins-custom,jicofo,jvb,jigasi,jibri}
-docker-compose -f docker-compose.yml -f jitsi.yml up routr routr-bootstrap wphone mediaserver redis rtpengine rtpengine-middleware web prosody jicofo jvb
